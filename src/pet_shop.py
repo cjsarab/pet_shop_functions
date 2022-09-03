a7c2515cf170f676f4ab1746d2fad1141ffc67a9
Cammy Smith <cjsarab1991@gmail.com>
2022-09-03 21:51:56 +0100

    Everything except integration complete

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -37,6 +37,12 @@ def find_pet_by_name(pet_shop,name):
     for pet in pet_shop["pets"]:
         if name == pet["name"]:
             return pet
+        
+def remove_pet_by_name(pet_shop,name):
+    for pet in pet_shop["pets"]:
+        if name == pet["name"]:
+            pet_shop["pets"].remove(pet)
+            return
 
 def add_pet_to_stock(pet_shop,new_pet):
     pet_shop["pets"].append(new_pet)
